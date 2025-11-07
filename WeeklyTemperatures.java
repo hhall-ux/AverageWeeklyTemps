7914a0739780acfdc9eb87f33f2dc935fb3e0b3a
hhall-ux <hanna.hall@csuglobal.edu>
2025-11-07 14:56:14 -0600

    Add files via upload

--- WeeklyTemperatures.java
+++ WeeklyTemperatures.java
@@ -7,11 +7,11 @@ public class WeeklyTemperatures {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        // Create two ArrayLists: one for days, one for temperatures
+        // Two array lists, one for days and one for temperatures
         ArrayList<String> days = new ArrayList<String>();
         ArrayList<Double> temps = new ArrayList<Double>();
 
-        // Add the days of the week
+        // Adding the days of the week
         days.add("Monday");
         days.add("Tuesday");
         days.add("Wednesday");
@@ -20,7 +20,7 @@ public class WeeklyTemperatures {
         days.add("Saturday");
         days.add("Sunday");
 
-        // Get a temperature for each day
+        // Getting a temperature for each day from user
         System.out.println("Enter the average temperature for each day of the week:");
         for (int i = 0; i < days.size(); i++) {
             System.out.print(days.get(i) + ": ");
@@ -31,18 +31,22 @@ public class WeeklyTemperatures {
         // Clear newline before using nextLine()
         scnr.nextLine();
 
+        // Instructions on how to use
         System.out.println();
         System.out.println("Enter a day of the week to see its temperature.");
         System.out.println("Type 'week' to show all and average, or 'exit' to quit.");
 
+        // Main loop for the interaction
         while (true) {
             System.out.print("\nYour choice: ");
             String input = scnr.nextLine();
 
+            // For if user wants to quit
             if (input.equals("exit")) {
                 System.out.println("Goodbye!");
                 break;
             }
+            // If user wants to see all temps and average
             else if (input.equals("week")) {
                 double sum = 0;
                 System.out.println("\nWeekly Temperatures:");
@@ -53,6 +57,7 @@ public class WeeklyTemperatures {
                 double average = sum / temps.size();
                 System.out.println("Weekly average temperature: " + average + "°");
             }
+            
             else if (input.equals("Monday") || input.equals("Tuesday") || input.equals("Wednesday")
                      || input.equals("Thursday") || input.equals("Friday")
                      || input.equals("Saturday") || input.equals("Sunday")) {
@@ -64,6 +69,8 @@ public class WeeklyTemperatures {
                     }
                 }
             }
+            
+            // If the user enters something invalid
             else {
                 System.out.println("Invalid entry. Try again.");
             }
